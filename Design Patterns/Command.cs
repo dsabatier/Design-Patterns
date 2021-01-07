733ff51834dcf2e047eb27d1358855088baab0cb
Dominic <dsabatier@gmail.com>
2021-01-06 23:05:52 -0700

    Command pattern

---
+++ Design Patterns/Command.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns.Command
+{
+    // Command
+    public interface IBattleAction
+    {
+        void PerformAction();
+    }
+
+    // ConcreteCommand
+    public class BasicAttackBattleAction : IBattleAction
+    {
+        private BattleActionController _controller;
+
+        public BasicAttackBattleAction(BattleActionController controller)
+        {
+            _controller = controller;
+        }
+
+        public void PerformAction()
+        {
+            Console.WriteLine("Basic attack!");
+        }
+    }
+
+    // ConcreteCommand
+    public class UseItemBattleAction : IBattleAction
+    {
+        private BattleActionController _controller;
+        private Item _item;
+
+        public UseItemBattleAction(BattleActionController controller, Item item)
+        {
+            _controller = controller;
+            _item = item;
+        }
+
+        public void PerformAction()
+        {
+            _item.Use();
+        }
+    }
+
+    // ConcreteCommand
+    public class EscapeBattleAction : IBattleAction
+    {
+        private BattleActionController _controller;
+
+        public EscapeBattleAction(BattleActionController controller)
+        {
+            _controller = controller;
+        }
+
+        public void PerformAction()
+        {
+            Console.WriteLine("Escaping!");
+        }
+    }
+
+    // Invoker
+    public class BattleActionController
+    {
+        private IBattleAction _basicAction;
+        private IBattleAction _specialAction;
+
+        public void SetBasicAction(IBattleAction action)
+        {
+            _basicAction = action;
+        }
+
+        public void SetSpecialAction(IBattleAction action)
+        {
+            _specialAction = action;
+        }
+
+        public void PerformBasicAction()
+        {
+            _basicAction.PerformAction();
+        }
+
+        public void PerformSpecialAction()
+        {
+            _specialAction.PerformAction();
+        }
+    }
+
+    // Receiver
+    public class PartyMember
+    {
+        private BattleActionController _actionController;
+
+        public PartyMember(BattleActionController actionController)
+        {
+            _actionController = actionController;
+        }
+
+        public void Battle()
+        {
+            _actionController.PerformBasicAction();
+            _actionController.PerformSpecialAction();
+        }
+
+    }
+
+    public class Item
+    {
+        public void Use()
+        {
+            Console.WriteLine("Item was used!");
+        }
+    }
+}
--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -4,6 +4,8 @@ using Design_Patterns.Observer;
 using Design_Patterns.Decorator;
 using Design_Patterns.AbstractFactory;
 using Design_Patterns.SingletonPattern;
+using Design_Patterns.Command;
+
 using System;
 
 namespace Design_Patterns
@@ -13,7 +15,7 @@ namespace Design_Patterns
         static void Main(string[] args)
         {
             // 1. strategy pattern
-
+            Console.WriteLine("1");
             // note that all of these are treated as actors rather than their specific type
             Actor knight = new Knight();
             knight.Attack();
@@ -42,7 +44,7 @@ namespace Design_Patterns
             }
 
             // 2. observer pattern
-
+            Console.WriteLine("2");
             Queen queen = new Queen();
             List<Zergling> zerglings = new List<Zergling>();
             for(int i = 0; i < 3; i++)
@@ -54,7 +56,7 @@ namespace Design_Patterns
             queen.SetState("DEAD");
 
             // 3. decorator
-
+            Console.WriteLine("3");
             // make a gun
             GunChassis machineGun = new HeavyGunChassis();
 
@@ -75,7 +77,7 @@ namespace Design_Patterns
             Console.WriteLine(sniper.GetRateOfFire());
 
             // 4. factory
-
+            Console.WriteLine("4");
             ConfigurationFactory assaultShipFactory = new AssaultConfigurationFactory();
             Spaceship assaultShip = new AssaultShip(assaultShipFactory);
             assaultShip.Activate();
@@ -88,10 +90,33 @@ namespace Design_Patterns
             // Fleet fleet = new Fleet(assaultShip, transportShip);
 
             // 5. singleton
+            Console.WriteLine("5");
 
             Singleton.Instance.DoAThing();
-            
 
+            // 6. command pattern
+            Console.WriteLine("6");
+
+            // set up controllers and actions for a party member
+            BattleActionController thiefActionController = new BattleActionController();
+            thiefActionController.SetBasicAction(
+                new BasicAttackBattleAction(thiefActionController));
+            thiefActionController.SetSpecialAction(
+                new EscapeBattleAction(thiefActionController));
+
+            // now we can create a thief..
+            PartyMember thief = new PartyMember(thiefActionController);
+            thief.Battle();
+
+            // .. and change his basic attack
+            thiefActionController.SetBasicAction(new UseItemBattleAction(thiefActionController, new Item()));
+            thief.Battle();
+
+            // we only ever need to know about the Battle method in the client (this file, in this example)
+            // so we can modify or create new actions without ever touching the class that would control
+            // when the thief needs to battle
+
+            
         }
     }
 }
